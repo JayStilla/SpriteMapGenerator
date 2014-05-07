0cec95ad7bf295197645719d526f96953e2c1ff4
Jayce Jastillana <jaycejastillana@gmail.com>
2014-05-06 17:11:23 -0700

    5/6/2014
    Can load multiple images and put them into a sheet, but is still a
    little glitchy right now.

---
+++ SpriteMapGenerator/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using System.Security;
+using System.Text.RegularExpressions; 
+
+
+namespace SpriteMapGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "image"; // Default file name 
+            dlg.DefaultExt = ".png"; // Default file extension 
+            dlg.Filter = "Image Files (*.png;*.BMP;*.JPG;*.GIF)|*.png;*.BMP;*.JPG;*.GIF"; // Filter files by extension 
+
+            dlg.Multiselect = true;
+            // Show open file dialog box 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results 
+            if (result == true)
+            {
+                // grab the file name of the image 
+                string[] filename = dlg.FileNames;
+
+                // and load the image 
+               
+                canvas1.LoadImage(filename[0]); // ... magics 
+                canvas1.GenSheet(filename); 
+            }
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
---
+++ SpriteMapGenerator/MyCanvas.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using System.IO;
+
+namespace SpriteMapGenerator
+{
+    public class MyCanvas : Canvas
+    {
+        BitmapImage background = null;
+
+        public void LoadImage(string filename)
+        {
+            background = new BitmapImage(new Uri(filename));
+            this.InvalidateVisual();
+        }
+        protected override void OnRender(DrawingContext dc)
+        {
+            if (background != null)
+            {
+                dc.DrawImage(background, new Rect(0, 0, background.PixelWidth, background.PixelHeight));
+            }
+        }
+        public void GenSheet(string[] images)
+        {
+            BitmapFrame[] frames = new BitmapFrame[images.Length];
+            int[] imageWidth = new int[images.Length];
+            int[] imageHeight = new int[images.Length];
+
+            int iW = images.Length;
+            int iH = images.Length; 
+
+            // Loads the images to tile (no need to specify PngBitmapDecoder, the correct decoder is automatically selected)
+            for (int i = 0; i < images.Length; i++)
+            {
+               frames[i] = BitmapDecoder.Create(new Uri(images[i]), BitmapCreateOptions.None, BitmapCacheOption.OnLoad).Frames.First();
+               imageWidth[i] = frames[i].PixelWidth;
+               imageHeight[i] = frames[i].PixelHeight; 
+            }
+            // Gets the size of the images (I assume each image has the same size)
+            for (int i = 0; i < images.Length; i++)
+            {
+                iW += frames[i].PixelWidth;
+                iH += frames[i].PixelHeight;
+            }
+
+            // Draws the images into a DrawingVisual component
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                drawingContext.DrawImage(frames[0], new Rect(0, 0, imageWidth[0], imageHeight[0]));
+                drawingContext.DrawImage(frames[1], new Rect(imageWidth[1], 0, imageWidth[1], imageHeight[1]));
+                drawingContext.DrawImage(frames[2], new Rect(0, imageHeight[2], imageWidth[2], imageHeight[2]));
+                drawingContext.DrawImage(frames[3], new Rect(imageWidth[3], imageHeight[3], imageWidth[3], imageHeight[3]));
+            }
+
+            // Converts the Visual (DrawingVisual) into a BitmapSource
+            RenderTargetBitmap bmp = new RenderTargetBitmap(iW * 2, iH * 2, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+
+            // Creates a PngBitmapEncoder and adds the BitmapSource to the frames of the encoder
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bmp));
+
+            string directory = Directory.GetCurrentDirectory();
+
+            // Saves the image into a file using the encoder
+            using (Stream stream = File.Create(directory + @"\tile.png"))
+                encoder.Save(stream);
+        }
+    }
+}
